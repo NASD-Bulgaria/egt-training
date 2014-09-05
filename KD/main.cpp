4ad086e5919fbf21b545a97ab5b74470a4b2f105
narscpp4 <kdaskalova@nars.bg>
2014-09-05 11:48:57 +0300

    hex

---
+++ KD/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	/*char ch ;
+	ch = 0x48;
+	cout << ch;
+	*/
+	/*string useOfQuotatins,useOfQ;
+	
+	useOfQuotatins = useOfQ = "The \"use\" of quotations causes difficulties. ";
+	cout << useOfQ;
+	*/
+	
+	int number;
+	number = 127345;
+	
+	
+	return 0;
+}
+
