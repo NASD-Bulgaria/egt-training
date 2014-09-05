80fde219ed3e93177edbd4daa7add8853b2c4bc8
Martin Markov <martinmarkov90@abv.bg>
2014-09-05 16:58:54 +0300

    05.09.2014
    STL

---
+++ MartinMarkov/STL Containers and Iterator/Lections_26_ex7_MM.cpp
@@ -0,0 +1,57 @@
+//============================================================================
+// Name        : Lections_26_ex7_MM.cpp
+// Author      : Martin
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+// 7. Write a program that removes from a given
+//sequence all negative numbers.
+
+#include <algorithm>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+template <class T>
+void print(T& c){
+   for( typename T::iterator i = c.begin(); i != c.end(); i++ ){
+      std::cout << *i << endl;
+   }
+}
+
+int main( )
+{
+   const float a[] = { 2, -3.5, 5.4, -3.4, -4, 34.78};
+   vector<float> data( a,a + sizeof( a ) / sizeof( a[0] ) );
+
+   cout << "DATA VECTOR HAS " << data.size() << " ELEMENTS\n";
+   print( data  );
+   list<float> l( data.begin(), data.end() );
+   cout << "\nSize of list before calling remove_if: "<< l.size() << endl;
+   l.remove_if( bind2nd( less<float>(), 0 ) );
+   cout << "Size of list after calling remove_if: " << l.size() << endl;
+   print( l );
+}
+/*
+ * Console
+DATA VECTOR HAS 6 ELEMENTS
+2
+-3.5
+5.4
+-3.4
+-4
+34.78
+
+Size of list before calling remove_if: 6
+Size of list after calling remove_if: 3
+2
+5.4
+34.78
+ */
